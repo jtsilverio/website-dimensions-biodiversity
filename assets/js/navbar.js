a29fc75f50f8b9dace686e517da3c05206d3603e
Jefferson Silverio <jefferson.silverio@syngenta.com>
2023-01-03 19:51:43 -0300

    P1 Enviada

--- assets/js/navbar.js
+++
@@ -1,7 +0,0 @@
-// // make the navbar sticky on scroll
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    var floatLimit = navbar.offsetTop + navbar.offsetHeight;
-    navbar.classList.toggle('fixed-top', window.pageYOffset > 1);
-    navbar.classList.toggle('menu__fixed', window.pageYOffset > 1);
-});

9a9e5321718fa13663cf8b3c8663725dea4b36b6
Jefferson Silverio <jefferson.silverio@syngenta.com>
2023-02-22 18:52:38 -0300

    V1

---
+++ scripts/resize_imgs.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import glob
+import tqdm
+
+files_path = glob.glob("assets/img/**/*.png")
+
+# Resize image to 900px width while maintaining aspect ratio if the width is greater than 900px
+for file in tqdm.tqdm(files_path):
+    img = Image.open(file)
+    width, height = img.size
+    if width > 900:
+        img = img.resize((900, int(height * 900 / width)), Image.ANTIALIAS)
+        img.save(file, optimize=True, quality=50)
+    else:
+        img.save(file, optimize=True, quality=50)

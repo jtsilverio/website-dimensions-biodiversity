eba4ea7c55a7b773c3ea70c14bba651066f2d7eb
Jefferson Silverio <jefferson.silverio@syngenta.com>
2023-02-24 20:22:11 -0300

    fix: typos, some images, resaved images in new format and quality

--- scripts/resize_imgs.py
+++ scripts/resize_imgs.py
@@ -2,7 +2,8 @@ from PIL import Image
 import glob
 import tqdm
 
-files_path = glob.glob("assets/img/**/*.png")
+files_path = glob.glob("assets/img/**/*.png", recursive=True)
+files_path = [path for path in files_path if "parallax" not in path]
 
 # Resize image to 900px width while maintaining aspect ratio if the width is greater than 900px
 for file in tqdm.tqdm(files_path):
@@ -10,6 +11,6 @@ for file in tqdm.tqdm(files_path):
     width, height = img.size
     if width > 900:
         img = img.resize((900, int(height * 900 / width)), Image.ANTIALIAS)
-        img.save(file, optimize=True, quality=50)
+        img.save(file, optimize=True, quality=100, method=6, format="WEBP")
     else:
-        img.save(file, optimize=True, quality=50)
+        img.save(file, optimize=True, quality=100, method=6, format="WEBP")
--- scripts/resize_parallax.py
+++ scripts/resize_parallax.py
@@ -2,13 +2,13 @@ from PIL import Image
 import glob
 import tqdm
 
-files_path = glob.glob("assets/img/parallax/**/*.png")
+files_path = glob.glob("assets/img/parallax/**/*.png", recursive=True)
 
 for file in tqdm.tqdm(files_path):
     img = Image.open(file)
-    width, height = img.size
+    # width, height = img.size
     # if width > 900:
     #     img = img.resize((1440, int(height * 1440 / width)), Image.ANTIALIAS)
     #     img.save(file, optimize=True, quality=50)
     # else:
-    img.save(file, optimize=True, quality=80, format="WEBP")
+    img.save(file, optimize=True, quality=80, method=6, format="WEBP")
